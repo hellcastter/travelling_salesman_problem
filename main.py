ab2fd1563ab37129018b26889afafd754bb0aebb
Victor Muryn <muryn.pn@ucu.edu.ua>
2022-12-21 12:55:17 +0200

    updated genetic algo

--- main.py
+++ main.py
@@ -1,6 +1,122 @@
 """ Traveling salesman problem """
+from random import sample, choice, randint
 from itertools import combinations
-from utils import CITIES_MAP, PATH, binary_without_vertex, distance, is_connected, vertexes_to_bits
+from typing import Iterable, List
+
+CITY = int | float
+CITIES_MAP = List[List[CITY]]
+PATH = List[int] | None
+
+def distance(x_city: int, y_city: int, cities_map: CITIES_MAP) -> int:
+    """
+    Distance between two cities
+    Args:
+        x_city (int): start city
+        y_city (int): end city
+        cities_map (List[List[int]]): city map
+    Returns:
+        int: distance
+    """
+    return cities_map[y_city][x_city]
+
+
+def vertexes_to_bits(combination: Iterable[int]) -> int:
+    """
+    Convert vertexes to bits by adding all binaries values
+
+    Args:
+        combination (Tuple[int]): Tuple of vertexes
+
+    Returns:
+        int: Binary representation
+
+    >>> vertexes_to_bits((1, 2, 3))
+    14
+    """
+    bits = 0
+    for i in combination:
+        bits |= 1 << i
+
+    return bits
+
+
+def binary_without_vertex(vertex: int, binary: int) -> int:
+    """
+    get combination without vertex element
+    we just set 0 in vertex-th place in binary
+
+    Args:
+        vertex (int): "deleted" vertex
+        binary (int): set represented in binary
+
+    Returns:
+        int: binary without element
+
+    >>> binary_without_vertex(3, 16)
+    16
+    """
+    return binary & ~(1 << vertex)
+
+
+def dfs(graph: CITIES_MAP) -> List[int]:
+    """
+    perform dfs on the graph and store its result
+    in a adjacency graph
+
+    Args:
+        graph (List[List[int]]): original graph
+
+    Returns:
+        List[int]: path
+
+    >>> dfs([[0, 5, -1], [5, 0, 3], [-1, 3, 0]])
+    [0, 1, 2]
+
+    >>> dfs([[0, 5, 0, -1], [5, 0, 3, -1], [0, 3, 0, -1], [-1, -1, -1, 0]])
+    [0, 1, 2]
+    """
+    result = [0]
+    stack = [0]
+
+    while stack:
+        key = stack[-1]
+        vertices = graph[key]
+
+        for index, vertex in enumerate(vertices):
+            if vertex in (0, -1):
+                continue
+
+            if index not in result:
+                result.append(index)
+                stack.append(index)
+                break
+        else:
+            # delete only in case we didn't find a vertex,
+            # which is not in result. In this case, if break statement
+            # wasn't called
+            del stack[-1]
+
+    return result
+
+
+def is_connected(graph: CITIES_MAP) -> bool:
+    """
+    Checks wether graph is connected
+
+    Args:
+        graph (List[List[int]]): original graph
+
+    Returns:
+        bool: is connected
+
+    >>> is_connected([[0, 5, -1], [5, 0, 3], [-1, 3, 0]])
+    True
+
+    >>> is_connected([[0, 5, 0, -1], [5, 0, 3, -1], [0, 3, 0, -1], [-1, -1, -1, 0]])
+    False
+    """
+    dfs_result = dfs(graph)
+    return len(dfs_result) == len(graph)
 
 
 def read_csv(file_name: str) -> CITIES_MAP:
@@ -216,3 +332,72 @@ def nna(cities_map: CITIES_MAP) -> PATH:
     result = [i + 1 for i in result]
 
     return result
+
+
+def calc_path_distance(path: List[int], cities_map: CITIES_MAP) -> int | float:
+    """
+    Calculate distance of path
+
+    Args:
+        path (List[int]): path (without first and last)
+        cities_map (CITIES_MAP): map
+
+    Returns:
+        int | float: distance of path
+    """
+    path_distance = 0
+
+    for index in range(1, len(path)):
+        city = path[index]
+        prev_city = path[index - 1]
+        path_distance += distance(prev_city, city, cities_map)
+
+    path_distance += distance(0, path[0], cities_map)
+    path_distance += distance(path[-1], 0, cities_map)
+
+    return path_distance
+
+
+def genetic(cities_map: CITIES_MAP, gnomes: int = 10, mutations: int = 100) -> PATH:
+    """
+    Searches the shortest way through all vertexes in graph going through
+    all vertexes only once in approximate way using genetic algorithm
+
+    Args:
+        cities_map (CITIES_MAP): Adjacency matrix representing distances between vertexes
+
+    Returns:
+        PATH: one of the shortest way if exists. On the other case None
+    """
+    if not is_connected(cities_map):
+        print('Graph is not connected.')
+        print('It is impossible to go through all vertexes.')
+        return None
+
+    length = len(cities_map)
+
+    paths = [sample(list(range(1, length)), length - 1) for _ in range(gnomes)]
+    paths = [(path, calc_path_distance(path, cities_map)) for path in paths]
+
+    for index, value in enumerate(paths):
+        path, path_distance = value
+
+        for _ in range(mutations):
+            first = randint(0, length - 2)
+
+            second_list = list(range(0, length - 1))
+            second_list.remove(first)
+            second = choice(second_list)
+
+            path_copy = path[:]
+            path_copy[first], path_copy[second] = path_copy[second], path_copy[first]
+
+            temp_distance = calc_path_distance(path_copy, cities_map)
+
+            if temp_distance < path_distance:
+                paths[index] = (path_copy, temp_distance)
+                path_distance = temp_distance
+                path = path_copy
+
+    path = min(paths, key=lambda x: x[1])[0]
+    return [1] + [i + 1 for i in path]  + [1]
